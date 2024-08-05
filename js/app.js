117c16249b3563d33d59bfadbbb05b440eabde0e
Humoyunmirzo2012 <webdevloper0509@gmail.com>
2024-08-05 10:33:14 +0500

    type-of

---
+++ js/app.js
@@ -0,0 +1,110 @@
+// //number
+// let num = 5;
+// console.log(typeof num);
+// console.log(typeof (num));
+
+// //string
+// let str = "5";
+// let str1 = "hello worold";
+// console.log(typeof str, typeof str1);
+
+// console.log(num+str);
+// console.log(num*str);
+
+// //boolean
+// let bool = false;
+// let bool1 = true;
+// console.log(typeof bool,typeof bool1);
+
+// //null
+// let data = null
+// console.log(typeof (data));
+
+// //underfined
+// let und;
+// console.log(und);
+// console.log(typeof und);
+
+// // symbol
+// let unequeID = Symbol("id")
+// console.log(typeof unequeID);
+
+// //BigInd
+// let big_int = BigInt(1)
+// console.log(typeof big_int);
+
+
+
+// //reference data types
+
+// //object
+// let obj = {
+//     name: "humoyunmirzo",
+//     age: 12,
+//     price: 1000
+// }
+// console.log(obj);
+// console.log(typeof obj);
+
+// //massivlar - Array
+
+// let arr = ['olma','anor', 'behi',5,10,15]
+// console.log(arr);
+// console.log(typeof arr); //object
+
+//fuction
+let funksiya = function(){
+    console.log("hello world");
+}
+console.log(funksiya);
+console.log(typeof funksiya);
+
+
+let numberVar = 100;
+console.log(numberVar);
+console.log(typeof numberVar);
+let stringVar = "JavaScript";
+console.log(numberVar);
+console.log(typeof stringVar);
+let booleanVar = false;
+console.log(booleanVar);
+console.log(typeof booleanVar);
+let undefinedVar;
+console.log(undefinedVar);
+console.log(typeof undefinedVar);
+let nullVar = null;
+console.log(nullVar);
+console.log(typeof nullVar);
+let objectVar = { language: "Uzbek", level: "Intermediate" };
+console.log(objectVar);
+console.log(typeof objectVar);
+let arrayVar = [10, 20, 30];
+console.log(arrayVar);
+console.log(typeof arrayVar);
+let functionVar = function() { console.log("Hello, world!"); };
+console.log(functionVar);
+console.log(typeof functionVar);
+
+
+
+
+// console.log(typeof 3.14); // "number"
+// console.log(typeof "123"); // "string"
+// console.log(typeof NaN); // "number"
+// console.log(typeof true); // "boolean"
+// console.log(typeof false); // "boolean"
+// console.log(typeof { key: "value" }); // "object"
+// console.log(typeof [1, 2, 3]); // "object"
+// console.log(typeof new Date()); // "object"
+// console.log(typeof function() {}); // "function"
+// console.log(typeof Symbol("id")); // "symbol"
+
+// let a;
+// let b = "Learning JavaScript";
+// let c = 42;
+// let d = true;
+// let e = { hobby: "coding" };
+// let f = [1, 2, 3];
+// let g = null;
+// let h = function() { return "Hello, world!"; };
+
